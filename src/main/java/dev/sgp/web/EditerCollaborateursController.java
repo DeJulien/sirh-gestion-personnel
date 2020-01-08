bfdb28d5c08b1ff4b2d4b0bba2ababcc8da85bf8
Julien Dewost <dewostjulien@gmail.com>
2020-01-08 14:13:40 +0100

    tp2 maj2

--- src/main/java/dev/sgp/web/EditerCollaborateursController.java
+++ src/main/java/dev/sgp/web/EditerCollaborateursController.java
@@ -13,21 +13,66 @@ public class EditerCollaborateursController extends HttpServlet
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException 
 	{
 	String matri = req.getParameter("matricule");
-
+	String titre = req.getParameter("titre");
+	String nom = req.getParameter("nom");
+	String prenom = req.getParameter("prenom");
+	boolean test=true;
 	resp.setContentType("text/html");
 	
 	if(matri==null)
 	{
 		resp.setStatus(400);
-		resp.getWriter().write("<h1>Un matricule est attendu</h1>");
-
+		matri=" ";
+		test=false;
 	}
-	else
+	if(titre==null)
 	{
-		resp.setStatus(200);
-		resp.getWriter().write("<h1>Liste des collaborateurs</h1> </br> <p>Matricule :"+matri+"</p>");
-
+		resp.setStatus(400);
+		titre=" ";
+		test=false;
 	}
+	if(nom==null)
+	{
+		resp.setStatus(400);
+		nom=" ";
+		test=false;
 	}
-
+	if(prenom==null)
+	{
+		resp.setStatus(400);
+		prenom=" ";
+		test=false;
+	}
+	
+	if(resp.getStatus()==400)
+	{
+			resp.getWriter().write("<h1>Liste des collaborateurs</h1> </br> <h2>Erreur Les paramètres suivants sont incorrects :</h2>"
+					+ "<ul>");
+			if(matri.equals(" "))
+				resp.getWriter().write("<li>matricule  "+ matri + "</li>");
+			if(titre.equals(" "))
+				resp.getWriter().write("<li>titre  "+ titre + "</li>");
+			if(nom.equals(" "))
+				resp.getWriter().write("<li>nom  "+ nom + "</li>");
+			if(prenom.equals(" "))
+				resp.getWriter().write("<li>prenom  "+ prenom + "</li>");
+			resp.getWriter().write("</ul>");
+	}
+	if(resp.getStatus()!=400)
+	{
+			resp.setStatus(200);
+			resp.getWriter().write("<h1>Liste des collaborateurs</h1> </br>"
+					+ "<ul>"
+					+ "<li>Matricule ="+ matri + "</li>"
+					+ "<li>titre ="+ titre + "</li>"
+					+ "<li>nom ="+ nom + "</li>"
+					+ "<li>prenom ="+ prenom + "</li>"
+					+ "</ul>");
+	}
+		
+	}
 }
+
+
+
+
